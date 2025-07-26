7b851a2baa81ab0dec8d08dd6249f3bbe18e3548
veerabhadra <veerabhadra1418@gmail.com>
2025-07-26 17:24:40 +0530

    Deleted all files to clean up the repo

--- tests/TC001_login_test.py
+++
@@ -1,7 +0,0 @@
-from pages.home_page import BasePage
-
-
-def test_login():
-    page = BasePage(driver)
-    page.open("http://localhost/opencart/")
-    assert "Your Store" in driver.title
--- tests/titletest.py
+++
@@ -1,7 +0,0 @@
-from pages.home_page import BasePage
-
-
-def test_title(driver):
-    page = BasePage(driver)
-    page.open("http://localhost/opencart/upload/")
-    assert "Your Store" in driver.title
